c00d2683fe2c1e7325619c6a1f79a421163dfa47
Chase Whitney <ChaseEWhitney@gmail.com>
2017-08-28 17:11:12 -0500

    some lists built out in tracking controller

--- server/public/scripts/controllers/tracking.controller.js
+++ server/public/scripts/controllers/tracking.controller.js
@@ -3,21 +3,53 @@ myApp.controller('TrackingController', function(UserService, $http) {
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
+  vm.dataList = [];
   vm.clientList = [];
+  vm.locationList = [];
+  vm.caseManagers = [];
   vm.clientGoals = [];
   vm.clientToView = {};
   vm.filters = ['Show All', 'Location', 'Case Manager', 'Option 3', 'Option 4', 'Option 5'];
 
+// builds list of clients to display on goal tracking page
+  buildLists = function(data){
+    console.log('in buildClientList with:', data);
+    var names = [];
+    var locations = [];
+    var cms = [];
+    for(var i = 0; i < data.length; i++){
+      if(!names.includes(data[i].client_name)){
+        vm.clientList.push(data[i]);
+        names.push(data[i].client_name);
+      }
+      if(!locations.includes(data[i].business_name)){
+        vm.locationList.push(data[i].business_name);
+        locations.push(data[i].business_name);
+      }
+      if(!cms.includes(data[i].staff_name)){
+        vm.caseManagers.push(data[i].staff_name);
+        cms.push(data[i].staff_name);
+      }
+    }
+  };
 
 
 //ted stuff
 getClients();
 
+
+
 function getClients(){
   $http.get('/tracking/getClients').then(function(response) {
     console.log(response.data);
-    vm.clientList = response.data;
+    vm.dataList = response.data;
+    buildLists(vm.dataList);
+console.log('vm.clientList:',vm.clientList);
+console.log('vm.locationList:',vm.locationList);
+console.log('vm.caseManagers:',vm.caseManagers);
+
   });
+
 }
 //end ted stuff
 
@@ -28,7 +60,6 @@ vm.showClientGoals = function(client){
     console.log('getGoals response:',response.data);
     vm.clientGoals = response.data;
   });
-console.log('end showClientGoals');
 };
 
 vm.filterClients = function(option){
