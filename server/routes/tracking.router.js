2b6b0b6fc187b8150d2f7ea527915dfb611798d1
th30d0rab1e <darthtlc@gmail.com>
2017-08-28 09:27:39 -0500

    finished getClients

--- server/routes/tracking.router.js
+++ server/routes/tracking.router.js
@@ -2,44 +2,44 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool.js');
 
-  function getLocation(req, res){
-    pool.connect(function(err, client, done, next) {
-      if(err) {
-        console.log("Error connecting: ", err);
-      }
-      var location;
-      client.query("select location from staff join users on staff.id = users.staff_id where users.id = "+ req.user.id+";",
-          function (err, result) {
-            client.end();
-            done();
-            if(err) {
-              console.log("Error inserting data: ", err);
-              next(err);
-            } else {
-              console.log('location', result.rows[0].location);
-              location = result.rows[0].location;
-
-            }
-          });
-    });
-    return location;
-  }
+  // function getLocation(req, res){
+  //   pool.connect(function(err, client, done, next) {
+  //     if(err) {
+  //       console.log("Error connecting: ", err);
+  //     }
+  //     var location;
+  //     client.query("select location from staff join users on staff.id = users.staff_id where users.id = "+ req.user.id+";",
+  //         function (err, result) {
+  //           client.end();
+  //           done();
+  //           if(err) {
+  //             console.log("Error inserting data: ", err);
+  //             next(err);
+  //           } else {
+  //             console.log('location', result.rows[0].location);
+  //             location = result.rows[0].location;
+  //
+  //           }
+  //         });
+  //   });
+  //   return location;
+  // }
 
 router.get('/getClients', function(req, res) {
   console.log('in server getting dem clients');
   console.log('CURRENT USER ID', req.user.id);
 
   //start getLocation
-   console.log(getLocation(req, res));
-   var location = getLocation(req, res);
-   console.log('LOCATION', location);
+  //  console.log(getLocation(req, res));
+  //  var location = getLocation(req, res);
+  //  console.log('LOCATION', location);
 
   pool.connect(function(err, client, done, next) {
     if(err) {
       console.log("Error connecting: ", err);
       next(err);
     }
-    client.query("select client.id, client.name, client.staff_id from client join staff on staff.id = client.staff_id where staff.location = " + location + ";",
+    client.query("select client.id, client.name, client.staff_id from client join staff on staff.id = client.staff_id join users on users.staff_id = staff.id where users.id = "+ req.user.id +";",
         function (err, result) {
           client.end();
           done();
