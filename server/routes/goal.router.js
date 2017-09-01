3dd63b862276a450fd969bcbd3dd1d035e366d65
th30d0rab1e <darthtlc@gmail.com>
2017-09-01 13:27:29 -0500

    client_name and staff_name

--- server/routes/goal.router.js
+++ server/routes/goal.router.js
@@ -86,7 +86,7 @@ router.get('/clients', function(req, res){
 router.get('/casemanager', function(req, res){
   console.log('In get route for client names. ');
 
-  var getCaseManagersQuery = 'SELECT "staff"."id", "staff"."name", "users"."role" FROM "staff" ' +
+  var getCaseManagersQuery = 'SELECT "staff"."id", "staff"."staff_name", "users"."role" FROM "staff" ' +
                  'JOIN "users" ON "staff"."id" = "users"."staff_id" WHERE "users"."role" = 2;';
                  console.log("Getting all Case Managers: ", getCaseManagersQuery);
 
