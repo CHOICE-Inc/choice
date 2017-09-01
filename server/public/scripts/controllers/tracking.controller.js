77c9021f7b91cc278350fd0d8e615a9977f28302
Chase Whitney <ChaseEWhitney@gmail.com>
2017-09-01 11:01:31 -0500

    client-side tracking fully functional, history still needs edit and delete functionality

--- server/public/scripts/controllers/tracking.controller.js
+++ server/public/scripts/controllers/tracking.controller.js
@@ -10,9 +10,10 @@ myApp.controller('TrackingController', function(UserService, $http, $mdToast) {
   vm.clientGoals = [];
   vm.clientToView = {};
   vm.trackingData = {};
+  vm.initials = "";
   vm.today = new Date();
   vm.filters = ['Show All', 'Location', 'Case Manager', 'Option 3', 'Option 4', 'Option 5'];
-vm.hidden=false;
+  vm.hidden=false;
 
   // builds list of clients to display on goal tracking page
   buildLists = function(data){
@@ -36,6 +37,30 @@ vm.hidden=false;
     }
   };
 
+  getLastUpdate = function(goal){
+    console.log('in getLastUpdate with:', goal);
+    var today = new Date();
+    var date = today.getFullYear() + "-" + getMonth(today.getMonth(), "month") + "-" + getMonth(today.getDate(), "day");
+    var amDate = goal.max_goal_date_am.toString().substring(0, 10);
+    var pmDate = goal.max_goal_date_pm.toString().substring(0, 10);
+    // console.log('STRING date is:', date);
+    // console.log('STRING amdate is:', amDate);
+    // console.log('STRING pmdate is:', pmDate);
+    if(date == amDate){
+      goal.amDone = true;
+    }
+    if(date == pmDate){
+      goal.pmDone = true;
+    }
+    // console.log('goal.amDone is:',goal.amDone);
+    // console.log('goal.pmDone is:',goal.pmDone);
+
+    if(goal.max_goal_date_am > goal.max_goal_date_pm){
+      return goal.max_goal_date_am;
+    } else {
+      return goal.max_goal_date_pm;
+    }
+  };
 
   //ted stuff
   getClients();
@@ -49,9 +74,9 @@ vm.hidden=false;
       console.log(response.data);
       vm.dataList = response.data;
       buildLists(vm.dataList);
-      console.log('vm.clientList:',vm.clientList);
-      console.log('vm.locationList:',vm.locationList);
-      console.log('vm.caseManagers:',vm.caseManagers);
+      // console.log('vm.clientList:',vm.clientList);
+      // console.log('vm.locationList:',vm.locationList);
+      // console.log('vm.caseManagers:',vm.caseManagers);
 
     });
 
@@ -66,6 +91,10 @@ vm.hidden=false;
     $http.get('/tracking/getGoals/' + client.clientid).then(function(response) {
       console.log('getGoals response:',response.data);
       vm.clientGoals = response.data;
+      for(var i = 0; i < vm.clientGoals.length; i++){
+        vm.clientGoals[i].lastUpdate = getLastUpdate(vm.clientGoals[i]);
+      }
+
     });
   };
 
@@ -73,9 +102,9 @@ vm.hidden=false;
   vm.filterClients = function(data){
     var tempList = angular.copy(data);
 
-    console.log('in filterClients with data:', data);
-    console.log('in filterClients with CM:', vm.filterCm);
-    console.log('in filterClients with JS:', vm.filterJs);
+    // console.log('in filterClients with data:', data);
+    // console.log('in filterClients with CM:', vm.filterCm);
+    // console.log('in filterClients with JS:', vm.filterJs);
 
     if(vm.caseManagers.includes(vm.filterCm)){
       for(var i = 0; i < tempList.length; i++){
@@ -107,14 +136,15 @@ vm.hidden=false;
 
 
   // submits goal tracking data
-  vm.trackGoal = function(goalId, amOrPm, completion, notes){
-    //need date
+  vm.trackGoal = function(goal){
+
     console.log('in trackGoal');
     var goalData = {
-      id: goalId,
-      time: amOrPm,
-      completion: completion,
-      notes: notes,
+      id: goal.goalid,
+      time: goal.amOrPm,
+      completion: goal.completion,
+      notes: goal.notes,
+      initials: vm.initials,
       date: new Date(),
     };
     console.log('sending goalData:', goalData);
@@ -123,17 +153,40 @@ vm.hidden=false;
       console.log('error: fill in time');
       swal(
         'Oops...',
-        'Please select AM or PM.',
+        'Please select either <b>AM</b> or <b>PM</b>.',
         'error'
       );
     } else if(goalData.completion == undefined){
       console.log('error: fill in completion');
       swal(
         'Oops...',
-        'Please select completion status.',
+        'Please select a <b>completion status</b>.',
         'error'
       );
-    } else {
+    } else if(goalData.time == "am" && goal.amDone == true){
+      console.log('am already done');
+      swal(
+        'Today\'s AM data has already been tracked.',
+        'To modify an entry, please click the <span style="color:blue"><b>Goal History</b></span> button.',
+        'error'
+      );
+    } else if(goalData.time == "pm" && goal.pmDone == true){
+      console.log('pm already done');
+      swal(
+        'Today\'s PM data has already been tracked.',
+        'To modify an entry, please click the <span style="color:blue"><b>Goal History</b></span> button.',
+        'error'
+      );
+    } else if(vm.initials == ""){
+      console.log('no initials');
+      swal(
+        'Please initial this tracking entry.',
+        'Enter your initials in the field labeled "initials."',
+        'error'
+      );
+    }
+
+    else {
       $http.post('/tracking/trackGoal/', goalData).then(function(response){
         console.log('Received response from trackGoal POST:', response);
         vm.showClientGoals(vm.clientToView);
@@ -147,14 +200,19 @@ vm.hidden=false;
   };
 
   vm.toGoalHistory = function(goal){
-    console.log('in toGoalHistory with goal:', goal);
-    console.log('goal hidden is:', goal.hidden);
+    // console.log('in toGoalHistory with goal:', goal);
+    // console.log('goal hidden is:', goal.hidden);
     if(goal.hidden == undefined){
       console.log('goal is undefined');
       goal.hidden = true;
+      vm.getGoalHistory(goal);
+    } else if(goal.hidden == false){
+      vm.getGoalHistory(goal);
+      goal.hidden = !goal.hidden;
     } else {
-    goal.hidden = !goal.hidden;
+      goal.hidden = !goal.hidden;
     }
+
   };
 
   vm.showToast = function(message, parentId){
@@ -170,43 +228,74 @@ vm.hidden=false;
     $mdToast.show(toast);
   };
 
-  vm.filterHistory = function(id){
-    console.log('in filterHistory with id:', id);
-    console.log('in filterHistory with startDate:', vm.historyStart);
-    console.log('in filterHistory with endDate:', vm.historyEnd);
+  vm.filterHistory = function(goal){
+    // console.log('in filterHistory with id:', id);
+    // console.log('in filterHistory with startDate:', vm.historyStart);
+    // console.log('in filterHistory with endDate:', vm.historyEnd);
 
-    if(vm.historyStart == undefined){
+    if(goal.historyStart == undefined){
       console.log('error: fill in start date');
       swal(
         'Oops...',
-        'Please enter a start date.',
+        'Please enter a <b>start date<b>.',
         'error'
       );
-    } else if(vm.historyEnd == undefined){
+    } else if(goal.historyEnd == undefined){
       console.log('error: fill in end date');
       swal(
         'Oops...',
-        'Please enter an end date.',
+        'Please enter an <b>end date<b>.',
         'error'
       );
+    } else if(goal.historyStart > goal.historyEnd) {
+      swal(
+      'Oops...',
+      'The start date cannot be a later date than the end date.',
+      'error'
+    );
+
+
     } else {
-      // $http.post('/tracking/trackGoal/', goalData).then(function(response){
-      //   console.log('Received response from trackGoal POST:', response);
-      //   vm.showClientGoals(vm.clientToView);
-      //   vm.showToast("Goal data submitted.", "footer");
-      // });
+      var newHistory = [];
+
+      for(var i = 0; i < goal.history.length; i++){
+        console.log('type of date_tracked', typeof goal.history[i].date_tracked);
+        if(typeof goal.history[i].date_tracked == "string"){
+          console.log('is string, converting');
+          goal.history[i].date_tracked = new Date(goal.history[i].date_tracked);
+          console.log('goal.history[i].date_tracked is:',goal.history[i].date_tracked);
+          console.log('typeof goal.history[i].date_tracked is:',typeof goal.history[i].date_tracked);
+        }
+
+
+        if(!(goal.history[i].date_tracked < goal.historyStart || goal.history[i].date_tracked > goal.historyEnd)){
+          newHistory.push(goal.history[i]);
+        }
+      }
+      goal.history = newHistory;
+      console.log('new goal.history is:', goal.history);
     }
 
   };
 
-  vm.getLastUpdate = function(goal){
-    console.log('in getLastUpdate with:', goal);
-    if(goal.max_goal_date_am > goal.max_goal_date_pm){
-      return goal.max_goal_date_am;
-    } else {
-      return goal.max_goal_date_pm;
-    }
+  getMonth = function(num, monthOrDay){
+    if(monthOrDay == "month"){num += 1;}
+    if(num < 10){return "0" + num;}
+    else {return num;}
   };
+
+
+
+  vm.getGoalHistory = function(goal){
+    console.log('in getGoalHistory');
+    $http.get('/tracking/getGoalHistory/' + goal.goal_id).then(function(response) {
+      console.log('getGoalHistory response:',response.data);
+      goal.history = response.data;
+      console.log('goal.history is:', goal.history);
+
+    });
+  };
+
 
   vm.test = function(){
     console.log('in test');
@@ -214,6 +303,7 @@ vm.hidden=false;
     console.log('am is:' ,vm.clientGoals[0].max_goal_date_am);
     console.log('pm is:' ,vm.clientGoals[0].max_goal_date_pm);
     console.log(vm.clientGoals[0].max_goal_date_pm < vm.clientGoals[0].max_goal_date_am);
+    console.log('getTime is:', vm.clientGoals[0].max_goal_date_pm);
   };
 
   vm.convertDate = function(date){
