a8176fbe178798cbf46c7f9efa78b53ad93487ce
Chase Whitney <ChaseEWhitney@gmail.com>
2017-09-05 16:59:30 -0500

    testing commit

--- server/public/scripts/controllers/client.controller.js
+++ server/public/scripts/controllers/client.controller.js
@@ -120,3 +120,26 @@ myApp.controller('ClientController', function(UserService, $http) {
 
 
 });
+// router.get('/getAllStaff', function(req, res) {
+//   console.log('in server getting dem staff');
+//
+//   pool.connect(function(err, client, done, next) {
+//     if(err) {
+//       console.log("Error connecting: ", err);
+//       next(err);
+//     }
+//     //join client, staff, and users to filter all cleints from user login
+//     client.query("select * from staff",
+//         function (err, result) {
+//           //client.end();
+//           done();
+//           if(err) {
+//             console.log("Error inserting data: ", err);
+//             //next(err);
+//           } else {
+//             console.log('RESULT ROWS', result.rows);
+//             res.send(result.rows);
+//           }
+//         });
+//   });
+// });
